23494b25c97e9d80b34e05bdfd5da7e7a8178c98
ashish123-paras <ashishchandila2100@gmail.com>
2026-01-13 09:26:10 +0530

    Update outputs.tf

--- terraform/part3-agents/outputs.tf
+++ terraform/part3-agents/outputs.tf
@@ -1,3 +1,4 @@
 output "control_plane_ip" {
-  value = google_compute_instance.bindplane.network_interface[0].access_config[0].nat_ip
+  value = data.terraform_remote_state.control_plane.outputs.control_plane_ip
 }
+
